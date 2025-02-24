0cb11972701437633c55ebba0e2058b4c74845f3
lazar <lazar@LZR-PC>
2025-02-24 11:56:35 -0300

    Adicionar arquivos de projeto.

---
+++ Products/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Products.Models
+{
+    public class Product
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ProductId { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [BindProperty(BinderType = typeof(DecimalModelBinder))]
+        public decimal Price { get; set; }
+        [Required]
+        public int StockQuantity { get; set; }
+    }
+}
---
+++ Products/Models/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Products.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
